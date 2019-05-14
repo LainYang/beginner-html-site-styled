370da279308f9017c577c46a8701333706592741
LainYang <zhumin1202@outlook.com>
2019-05-14 16:25:13 +0800

    JavaScript练习

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+﻿// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+};
+//图片切换代码
+
+function setHeading(name) {
+    let myheading = document.querySelector('h1');
+    myheading.textContent = 'Mozilla酷毙了，' + name + '!';
+}
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    localStorage.setItem('name', myName);
+    setHeading(myName);
+}
+
+let storedName = localStorage.getItem('name');
+if(!storedName) {
+    setUserName();
+} else {
+    setHeading(storedName);
+}
+
+let myButton = document.querySelector('button');
+myButton.onclick = setUserName;
+//个性化欢迎信息
